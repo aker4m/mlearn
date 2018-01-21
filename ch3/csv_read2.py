2c7e88b292e4d9db30cc3c81affe43edcb82c0cd
aker <mageaker@hotmail.com>
2018-01-22 00:41:17 +0900

    180122 csv

---
+++ ch3/csv_read2.py
@@ -0,0 +1,8 @@
+import csv, codecs
+
+filename = 'list-euckr.csv'
+fp = codecs.open(filename, 'r', 'utf-8')
+
+reader = csv.reader(fp, delimiter=',', quotechar="'")
+for cells in reader:
+    print(cells[1], cells[2])
